c92a2c227f0489e0e704bbdc1a4713a666da4b7e
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-06 19:14:15 -0300

    refactor(routePutUser)

--- src/routes/admin/user/routePutUser.js
+++ src/routes/admin/user/routePutUser.js
@@ -10,10 +10,10 @@ router.put(
     async (req, res, next) => {
         try {
             const { id } = req.params;
-            const { userName, role, status } = req.body;
+            const { userName, role, password, status } = req.body;
             // console.log('put /user/:id req.body: ', req.body);
 
-            const user = await editUser(id, userName, role, status);
+            const user = await editUser(id, userName,password, role, status);
 
             res.status(200).json(user);
         } catch (error) {
