fb4baed957e3a2cabf9a21cb9b72e13410387484
goya0310 <108840268+goya0310@users.noreply.github.com>
2023-03-01 13:11:16 -0300

    Update controllerMercadoPago.js

--- src/controllers/client/controllerMercadoPago.js
+++ src/controllers/client/controllerMercadoPago.js
@@ -1,14 +1,27 @@
 const mercadopago = require("mercadopago");
 // si quiero validar con bd el customer
 // const { Customer } = require("../../models/Customer");
-const { Order } = require("../../db.js");
+const { Order, Product, OrderItem } = require("../../db.js");
+const { sequelize } = require("../../db");
 
 mercadopago.configure({
   access_token: process.env.ACCESS_TOKEN,
 });
 
+let backRedirectUrl = "";
+let frontRedirectUrl = "";
+
+if (process.env.NODE_ENV === "TEST") {
+  backRedirectUrl = "http://localhost:3001";
+  frontRedirectUrl = "http://localhost:3000";
+} else {
+  backRedirectUrl = "https://pg-henry.up.railway.app";
+  frontRedirectUrl = "https://pg-front-henry.vercel.app";
+}
+
 const payment = async (req, res, next) => {
   const order = req.body;
+
   try {
     // VALIDACION CON DB
     // async function getById(id) {
@@ -26,6 +39,7 @@ const payment = async (req, res, next) => {
     // FIN VALIDACION DB
 
     // Configuro las preferencias requeridas por Mercadopago
+
     const preference = {
       items: [
         {
@@ -42,9 +56,9 @@ const payment = async (req, res, next) => {
       },
       back_urls: {
         //rutas de acuerdo a como haya salido la transacion
-        success: "http://localhost:" + process.env.PORT + "/pay/",
-        failure: "http://localhost:" + process.env.PORT + "/pay/",
-        pending: "http://localhost:" + process.env.PORT + "/pay/",
+        success: backRedirectUrl + "/pay/",
+        failure: backRedirectUrl + "/pay/",
+        pending: frontRedirectUrl,
       },
       statement_descriptor: "Yazz Shows",
       auto_return: "approved",
@@ -52,16 +66,74 @@ const payment = async (req, res, next) => {
       binary_mode: true,
     };
 
-    return await mercadopago.preferences
-      .create(preference)
-      .then((response) => res.status(200).send({ response }));
-    //se debe guardar en db el id que se obtiene de MP y se usa en getPaymentByID
-    // return res.status(200).json({ id: mp.body.id, items: req.body.items });
+    return await mercadopago.preferences.create(preference).then((response) => {
+      res.status(200).send({ response });
+    });
   } catch (error) {
     console.log(error);
     // next(error);
   }
 };
+
+async function updateStock(orderId) {
+  let transaction;
+  // Obtener todos los items de la orden
+  const orderItems = await OrderItem.findAll({
+    where: { orderId },
+    attributes: ["productId", "quantity"],
+  });
+
+  try {
+    // Iniciar una transacción
+    transaction = await sequelize.transaction();
+
+    // Validar que hay al menos un item de la orden
+    if (orderItems.length === 0) {
+      throw new Error("La orden no tiene items.");
+    }
+
+    // Actualizar el stock de cada producto correspondiente
+    await Promise.all(
+      orderItems.map(async (orderItem) => {
+        // Obtener el producto correspondiente al item de la orden
+        const productToUpdate = await Product.findByPk(orderItem.productId, {
+          transaction,
+        });
+
+        // Guardar una copia temporal del stock original
+        const originalStock = productToUpdate.stock;
+
+        // Validar el nuevo stock
+        if (originalStock < orderItem.quantity) {
+          throw new Error(
+            `Stock insuficiente para el producto ${productToUpdate.id}`
+          );
+        }
+        // Actualizar el stock
+        productToUpdate.stock = originalStock - orderItem.quantity;
+
+        // Para ver si actualiza bien el stock
+        console.log(
+          `El nuevo stock del Producto con ID ${orderItem.productId}: `,
+          productToUpdate.stock
+        );
+
+        // Guardar los cambios
+        await productToUpdate.save({ transaction });
+      })
+    );
+
+    // Confirmar la transacción
+    await transaction.commit();
+  } catch (error) {
+    // // Si hay un error, revertir la transacción y restaurar el stock original
+    // if (transaction) {
+    //   await transaction.rollback();
+    // }
+    console.error(error);
+    throw new Error("Orden cancelada por falta de stock");
+  }
+}
 
 // para recibir la info del pago
 const getPaymentInfo = async (req, res, next) => {
@@ -75,20 +147,42 @@ const getPaymentInfo = async (req, res, next) => {
       order.payment_id = payment_id;
       order.payment_status = payment_status;
       order.merchant_order_id = merchant_order_id;
-      order.payment_status === "approved"
-        ? (order.status = "Completed")
-        : (order.status = "Canceled");
+      if (order.payment_status === "null")
+        return res.redirect(frontRedirectUrl);
 
+      const changeStatus = async () => {
+        if (order.payment_status === "approved") {
+          try {
+            await updateStock(external_reference);
+          } catch (error) {
+            // order.status = "Canceled";
+            console.log(error);
+          }
+        } else {
+          order.status = "Canceled";
+        }
+      };
+
+      changeStatus();
+      if (order.status === "Created") {
+        order.status = "Canceled";
+        order
+          .save()
+          .then((_) => {
+            return res.redirect(frontRedirectUrl + "/PaymentError");
+          })
+          .catch((err) => {
+            console.error("error al guardar paymentError", err);
+          });
+      }
+      if (order.status !== "Canceled") order.status = "Completed";
       order
         .save()
         .then((_) => {
-          return res.redirect("http://localhost:3000");
+          return res.redirect(frontRedirectUrl);
         })
         .catch((err) => {
           console.error("error al guardar", err);
-          // return res.redirect(
-          //   `http://localhost:3000/?error=${err}&where=saving`
-          // );
         });
     });
   } catch (error) {
